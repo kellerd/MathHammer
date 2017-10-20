ef29c6b30dabd792b8a44d7329228be2208dfaf2
kellerd <daniel.allan.keller@gmail.com>
2017-10-19 20:56:47 -0400

    Change dist tests

---
+++ test/DistTest.fs
@@ -0,0 +1,15 @@
+module DistTest
+open Distribution
+open Expecto                                       
+open GameActions.Primitives.Types
+let dist = [1;2;3;4;5;6] |> List.map (fun i -> if i > 2 then i |> Int |> Check.Pass
+                                               else i |> Int |> Check.Fail ) |> uniformDistribution
+[<Tests>]
+let tests = 
+    testList "Distribution Tests" [
+        test "Check Add with ints" {
+            let result = dist |> List.map fst |> List.reduce (Check.add)
+            let expected = ([1..6] |> List.sum |> Int |> Check.Pass)
+            Expect.equal result expected "Adding up dice is same as adding integers" 
+        }
+    ]
--- test/DistTest.fsx
+++
@@ -1,8 +0,0 @@
-#load "LoadModules.fsx"
-open Check
-open Distribution
-open GameActions.Primitives.Types
-let dist = [1;2;3;4;5;6] |> List.map (fun i -> if i > 2 then i |> Int |> Pass
-                                               else i |> Int |> Fail ) |> uniformDistribution
-
-dist |> List.map fst |> List.reduce (Check.add) = ([1..6] |> List.sum |> Int |> Pass)
