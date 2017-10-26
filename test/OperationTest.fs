8481dd2f9622b65580dc81931e54282129c05255
Dan Keller <daniel.allan.keller@gmail.com>
2017-10-26 08:05:18 -0400

    Move some operation tests

--- test/OperationTest.fs
+++ test/OperationTest.fs
@@ -23,53 +23,55 @@ let tests =
        
     ]
 
+[<Tests>]
+let ``Operation Tests`` = 
+    testList "Operation Tests" [
+        let v = Value(Int(3))
+        //Let x = 3 returns 3 as well as binding to environment
+        let retValueIsSame v f =
+            let evaled = Let("x", v ,Var ("x")) |> f |> evalOp standardCall Map.empty<_,_> 
+            let evaled' = v |> f |> evalOp standardCall Map.empty<_,_> 
+            evaled ==? evaled'
+        yield test "Return value is same evaluated without normalization" { retValueIsSame v id}
+        yield test "Return value is same evaluated with normalization" {retValueIsSame v normalizeOp}
+        //Let x = some number in
+        //x + some other number
+        let addition x y  =    
+            let v = Value(Int(x))
+            let (Value(Dist(d))) =
+                Let("x", v ,App(Call Total, ParamArray([Value(Int(y));Var ("x")])))
+                |> evalOp standardCall Map.empty<_,_>
+            let expected = Distribution.always (Int(x + y))
+            printfn "Is %A = %A" d expected
+            d = expected
+        //Let x = 6
+        //Total of x is 6
+        let totalOfXIsX x = 
+            let v = Value(Int(x))
+            let (Value(Dist(d))) =
+                Let("x", v ,App(Call Total, v)) 
+                |> evalOp standardCall Map.empty<_,_>
+            let expected = Distribution.always (Int(x))
+            printfn "Is %A = %A" d expected
+            d = expected
+        //Count of one passed result is 1
+        let countOfOneXIsOneX x = 
+            let v = Value(Check(Check.Pass(Int(x))))
+            let (Value(Dist(d))) =
+                Let("x", v ,App(Call Count, v)) 
+                |> evalOp standardCall Map.empty<_,_>
+            let expected = Distribution.always (Check(Check.Tuple (Int(1),Int(0))))
+            printfn "Is %A = %A" d expected
+            d = expected
 
+        totalOfXIsX 6  
+        addition 3 9    
+        countOfOneXIsOneX 6  
 
-// let v = Value(Int(3))
-// //Let x = 3 returns 3 as well as binding to environment
-// let retValueIsSame v f =
-//     let evaled = Let("x", v ,Var ("x")) |> f |> evalOp standardCall Map.empty<_,_> 
-//     let evaled' = v |> f |> evalOp standardCall Map.empty<_,_> 
-//     evaled = evaled'
-// retValueIsSame v id
-// retValueIsSame v normalizeOp
-// //Let x = some number in
-// //x + some other number
-// let addition x y  =    
-//     let v = Value(Int(x))
-//     let (Value(Dist(d))) =
-//         Let("x", v ,App(Call Total, ParamArray([Value(Int(y));Var ("x")])))
-//         |> evalOp standardCall Map.empty<_,_>
-//     let expected = Distribution.always (Int(x + y))
-//     printfn "Is %A = %A" d expected
-//     d = expected
-// //Let x = 6
-// //Total of x is 6
-// let totalOfXIsX x = 
-//     let v = Value(Int(x))
-//     let (Value(Dist(d))) =
-//         Let("x", v ,App(Call Total, v)) 
-//         |> evalOp standardCall Map.empty<_,_>
-//     let expected = Distribution.always (Int(x))
-//     printfn "Is %A = %A" d expected
-//     d = expected
-// //Count of one passed result is 1
-// let countOfOneXIsOneX x = 
-//     let v = Value(Check(Check.Pass(Int(x))))
-//     let (Value(Dist(d))) =
-//         Let("x", v ,App(Call Count, v)) 
-//         |> evalOp standardCall Map.empty<_,_>
-//     let expected = Distribution.always (Check(Check.Tuple (Int(1),Int(0))))
-//     printfn "Is %A = %A" d expected
-//     d = expected
-
-// totalOfXIsX 6  
-// addition 3 9    
-// countOfOneXIsOneX 6  
-
-// let unfoldD6 = 
-//     let WS = dPlus D6 3
-//     let A = vInt 3
-//     let unfold = unfoldOp WS A
-//     unfold 
-//     |> evalOp standardCall Map.empty<_,_>
+        let unfoldD6 = 
+            let WS = dPlus D6 3
+            let A = vInt 3
+            let unfold = unfoldOp WS A
+            unfold 
+            |> evalOp standardCall Map.empty<_,_>
+            ]
